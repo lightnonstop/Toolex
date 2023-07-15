105a5335e69e0cbc32308ef796fc611c593728f2
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 14:40:48 +0100

    set icon size on screen width

--- src/components/NavigationComponent.tsx
+++ src/components/NavigationComponent.tsx
@@ -6,7 +6,7 @@ interface NavigationComponentProps{
 function NavigationComponent({ title, subtitle, icon }: NavigationComponentProps) {
   return (
     <button className="flex flex-row gap-4 cursor-pointer">
-      <div className="flex flex-row items-center justify-center bg-lightSecondary rounded-full w-12 max-sm:w-10 h-12 max-sm:h-10">
+      <div className="flex flex-row items-center justify-center bg-lightSecondary rounded-full w-12 max-[1146px]:w-9 h-12 max-[1146px]:h-9">
         {icon}
       </div>
       <div className={`flex flex-col text-start ${subtitle !== '123-456-7890' ? "max-tablet:hidden" : ""}`}>
