e3bed3195401b9397fac9b2a321bb74d2e1b8b79
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 05:51:45 +0100

    add custom tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: ',hsl(41.84,98.28%,54.51%)',
+        secondary: 'hsl(198.62,38.67%,14.71%)',
+        white: 'hsl(0,0%,100%)',
+        dimWhite: 'hsl(0,0%,96.86%)',
+        grey: 'hsl(234.15,18.39%,56.27%)',
+        dimGrey: 'hsl(0,0%,86.67%)'
+      },
+    },
+    screens: {
+      mobile: '480px',
+      tablet: '991px',
+      desktop: '1007px',
+    }
   },
   plugins: [],
 }
