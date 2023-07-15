212768c1717c8ddc771197326b2150f7d9153818
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 06:54:16 +0100

    export new pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as SpecialProducts } from './SpecialProducts';
+export { default as Brands } from './Brands';
+export { default as ContactInformation } from './ContactInformation';
+export { default as ContactSiteMap } from './ContactSiteMap';
