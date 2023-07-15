897863574273d863a8b74be9aa8486afda051991
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 11:49:45 +0100

    cursor to pointer on hover

--- src/components/NavigationComponent.tsx
+++ src/components/NavigationComponent.tsx
@@ -5,7 +5,7 @@ interface NavigationComponentProps{
 }
 function NavigationComponent({ title, subtitle, icon }: NavigationComponentProps) {
   return (
-    <button className="flex flex-row gap-4">
+    <button className="flex flex-row gap-4 cursor-pointer">
       <div className="flex flex-row items-center justify-center bg-lightSecondary rounded-full w-12 max-sm:w-10 h-12 max-sm:h-10">
         {icon}
       </div>
