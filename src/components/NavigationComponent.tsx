ac0b596209d49a14cab54502e473ffe2317706cb
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 11:41:51 +0100

    add navigation component

---
+++ src/components/NavigationComponent.tsx
@@ -0,0 +1,19 @@
+interface NavigationComponentProps{
+    title: string;
+    subtitle: string;
+    icon: React.ReactElement;
+}
+function NavigationComponent({ title, subtitle, icon }: NavigationComponentProps) {
+  return (
+    <button className="flex flex-row gap-4">
+      <div className="flex flex-row items-center justify-center bg-lightSecondary rounded-full w-12 max-sm:w-10 h-12 max-sm:h-10">
+        {icon}
+      </div>
+      <div className={`flex flex-col text-start ${subtitle !== '123-456-7890' ? "max-tablet:hidden" : ""}`}>
+        <p className="text-white capitalize font-semibold max-[1146px]:text-[0.9rem]">{title}</p>
+        <p className="text-white capitalize max-[1146px]:text-sm">{subtitle}</p>
+      </div>
+    </button>
+  )
+}
+export default NavigationComponent
