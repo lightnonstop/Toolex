ff7cd4adb9d0e8b461f235734679ab3a322bbc83
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 07:00:05 +0100

    fix missing title component import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Title } from './Title';
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as LeftSidebar } from './LeftSidebar';
