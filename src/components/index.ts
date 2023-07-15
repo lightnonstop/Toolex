73798387c8a30fe5dd7633da51016e71bcdc5599
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 06:53:52 +0100

    export explicitly sidebar, navbar and footer components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Layout } from './Layout';
-export { default as Title } from './Title';
+export { default as Navbar } from './Navbar';
+export { default as LeftSidebar } from './LeftSidebar';
+export { default as RightSidebar } from './RightSidebar';
+export { default as Footer } from './Footer';
