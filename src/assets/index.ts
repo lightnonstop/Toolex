b0ac9988460598956c0779227c174fa06072fc83
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 19:54:01 +0100

    import new images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,23 @@
 import logo from './logo.png';
+import image1 from './1-20x20.png';
+import image2 from './2-20x20.png';
+import image3 from './3-20x20.png';
+import image4 from './4-20x20.png';
+import image5 from './5-20x20.png';
+import image6 from './6-20x20.png';
+import image9 from './9-20x20.png';
+import image10 from './10-20x20.png';
+import image11 from './11-20x20.png';
 
 export {
     logo,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image9,
+    image10,
+    image11,
 }
