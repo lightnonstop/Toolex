8e5d75e592778f34b191b88eb57c96e07c21ceb8
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 06:43:54 +0100

    add app pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./components";
-import { Home } from "./pages";
+import { Brands, ContactInformation, ContactSiteMap, Home, SpecialProducts } from "./pages";
+import Blogs from "./pages/Blogs";
 function App() {
-
   return (
-    <Router>
+      <>
+     <Router>
       <Routes>
-        <Route element={<Layout />}>
-          <Route index path="/" element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
+          <Route element={<Layout />}>
+            <Route index path="/" element={<Home />} />
+            <Route index path="/product/special" element={<SpecialProducts />} />
+            <Route index path="/product/brands" element={<Brands />} />
+            <Route index path="/information/contact" element={<ContactInformation />} />
+            <Route index path="/information/sitemap" element={<ContactSiteMap />} />
+            <Route index path="/blogs" element={<Blogs />} />
+          </Route>
+        </Routes>
+     </Router>
+      </>
   )
 }
 
