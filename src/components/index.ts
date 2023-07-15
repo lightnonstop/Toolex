1606edc5b7a058eada8eb7c3ece0fb566f3b5487
lightnonstop <umichaeledet003@gmail.com>
2023-07-15 07:08:42 +0100

    export navigation component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Navbar } from './Navbar';
 export { default as LeftSidebar } from './LeftSidebar';
 export { default as RightSidebar } from './RightSidebar';
 export { default as Footer } from './Footer';
+export { default as NavigationComponent } from './NavigationComponent';
